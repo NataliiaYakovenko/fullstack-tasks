37e35e9776a048d0c598d059190af4c85ae48648
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-25 22:49:36 +0300

    Add patern files

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = require("./routes/index");
+const { errorHandlers } = require("./middlewares/errorHandlers");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+app.use(errorHandlers);
+
+const corsOption = {
+  origin: "*",
+};
+app.use(cors(corsOption));
+
+module.exports = app;
